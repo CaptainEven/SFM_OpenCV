19dc3faf9bd56afdc1746766372399d93e8a10b3
Even <765305261@qq.com>
2020-09-30 17:18:37 +0800

    Add Bundle adjustment to SFM.

--- OpenCV_SFM/NViewReconstuct.cpp
+++ OpenCV_SFM/NViewReconstuct.cpp
@@ -8,11 +8,15 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/features2d.hpp>
 
+#include "ceres/ceres.h" 
+#include "ceres/rotation.h"
+#include "glog/logging.h" 
+
 
 using namespace std;
 using namespace cv;
 
-
+// --------------------
 // void getAllFiles(string path, vector<string>& files, string format);
 
 void init_structure(
@@ -20,7 +24,7 @@ void init_structure(
 	const vector<vector<KeyPoint>>& key_points_for_all,
 	const vector<vector<Vec3b>>& colors_for_all,
 	const vector<vector<DMatch>>& matches_for_all,
-	vector<Point3f>& structure,
+	vector<Point3d>& structure,
 	vector<vector<int>>& correspond_struct_idx,
 	vector<Vec3b>& colors,
 	vector<Mat>& rotations,
@@ -40,13 +44,28 @@ void get_matched_colors(
 	vector<Vec3b>& out_c2
 );
 bool find_transform(const Mat& K, const vector<Point2f>& p1, const vector<Point2f>& p2, Mat& R, Mat& T, Mat& mask);
-void maskout_points(vector<Point2f>& p1, Mat& mask);
-void maskout_colors(vector<Vec3b>& p1, Mat& mask);
-void reconstruct(const Mat& K, Mat& R1, Mat& T1, Mat& R2, Mat& T2, vector<Point2f>& p1, vector<Point2f>& p2, vector<Point3f>& structure);
+
+void maskout_points(const Mat& mask, vector<Point2f>& p1);
+void maskout_2d_pts_pair(const Mat& mask, vector<Point2f>& pts1, vector<Point2f>& pts2);
+
+void maskout_colors(const Mat& mask, vector<Vec3b>& p1);
+
+void bundle_adjustment(
+	Mat& intrinsic,
+	vector<Mat>& extrinsics,
+	vector<vector<int>>& correspond_struct_idx,
+	vector<vector<KeyPoint>>& key_points_for_all,
+	vector<Point3d>& structure
+);
+
+void reconstruct(const Mat& K,
+	Mat& R1, Mat& T1, Mat& R2, Mat& T2,
+	vector<Point2f>& p1, vector<Point2f>& p2,
+	vector<Point3d>& structure);
 void get_objpoints_and_imgpoints(
 	vector<DMatch>& matches,
 	vector<int>& struct_indices,
-	vector<Point3f>& structure,
+	vector<Point3d>& structure,
 	vector<KeyPoint>& key_points,
 	vector<Point3f>& object_points,
 	vector<Point2f>& image_points);
@@ -54,8 +73,8 @@ void fusion_structure(
 	vector<DMatch>& matches,
 	vector<int>& struct_indices,
 	vector<int>& next_struct_indices,
-	vector<Point3f>& structure,
-	vector<Point3f>& next_structure,
+	vector<Point3d>& structure,
+	vector<Point3d>& next_structure,
 	vector<Vec3b>& colors,
 	vector<Vec3b>& next_colors);
 void extract_features(
@@ -66,7 +85,56 @@ void extract_features(
 );
 void match_features(const Mat& query, const Mat& train, vector<DMatch>& matches);
 void match_features(const vector<Mat>& descriptor_for_all, vector<vector<DMatch>>& matches_for_all);
-void save_structure(string file_name, vector<Mat>& rotations, vector<Mat>& motions, vector<Point3f>& structure, vector<Vec3b>& colors);
+void save_structure(string file_name,
+	vector<Mat>& rotations, 
+	vector<Mat>& motions, 
+	vector<Point3d>& structure,
+	vector<Vec3b>& colors);
+
+// --------------------
+struct ReprojectCost
+{
+	cv::Point2d observation;
+
+	ReprojectCost(cv::Point2d& observation)
+		: observation(observation)
+	{
+	}
+
+	template <typename T>
+	bool operator()(const T* const intrinsic, const T* const extrinsic, const T* const pos3d, T* residuals) const
+	{
+		const T* r = extrinsic;
+		const T* t = &extrinsic[3];
+
+		T pos_proj[3];
+		ceres::AngleAxisRotatePoint(r, pos3d, pos_proj);
+
+		// Apply the camera translation
+		pos_proj[0] += t[0];
+		pos_proj[1] += t[1];
+		pos_proj[2] += t[2];
+
+		const T x = pos_proj[0] / pos_proj[2];
+		const T y = pos_proj[1] / pos_proj[2];
+
+		const T fx = intrinsic[0];
+		const T fy = intrinsic[1];
+		const T cx = intrinsic[2];
+		const T cy = intrinsic[3];
+
+		// Apply intrinsic
+		const T u = fx * x + cx;
+		const T v = fy * y + cy;
+
+		residuals[0] = u - T(observation.x);
+		residuals[1] = v - T(observation.y);
+
+		return true;
+	}
+};
+
+// --------------------
 
 const string dir = "../Images/";
 
@@ -102,7 +170,7 @@ int main(int argc, char** argv)
 	// 对所有图像进行顺次的特征匹配
 	match_features(descriptor_for_all, matches_for_all);
 
-	vector<Point3f> structure;
+	vector<Point3d> structure;
 	vector<vector<int>> correspond_struct_idx;	// 保存第i副图像中第j特征点对应的structure中点的索引
 	vector<Vec3b> colors;  // 3个字节表示一个RGB或BGR颜色
 	vector<Mat> rotations;
@@ -155,7 +223,7 @@ int main(int argc, char** argv)
 		get_matched_colors(colors_for_all[i], colors_for_all[i + 1], matches_for_all[i], c1, c2);
 
 		// 根据之前求得的R, T进行三维重建
-		vector<Point3f> next_structure;
+		vector<Point3d> next_structure;
 		reconstruct(K, rotations[i], motions[i], R, T, p1, p2, next_structure);
 		printf("Frame %d reconstructed.\n", i);
 
@@ -177,10 +245,30 @@ int main(int argc, char** argv)
 	cout << "Done." << endl;
 	getchar();
 
+	Mat intrinsic(Matx41d(K.at<double>(0, 0), K.at<double>(1, 1), K.at<double>(0, 2), K.at<double>(1, 2)));
+	vector<Mat> extrinsics;
+	for (size_t i = 0; i < rotations.size(); ++i)
+	{
+		Mat extrinsic(6, 1, CV_64FC1);
+		Mat r;
+		Rodrigues(rotations[i], r);
+
+		r.copyTo(extrinsic.rowRange(0, 3));
+		motions[i].copyTo(extrinsic.rowRange(3, 6));
+
+		extrinsics.push_back(extrinsic);
+	}
+
+	bundle_adjustment(intrinsic, extrinsics, correspond_struct_idx, key_points_for_all, structure);
+
 	return 0;
 }
 
-void save_structure(string file_name, vector<Mat>& rotations, vector<Mat>& motions, vector<Point3f>& structure, vector<Vec3b>& colors)
+void save_structure(string file_name,
+	vector<Mat>& rotations,
+	vector<Mat>& motions,
+	vector<Point3d>& structure,
+	vector<Vec3b>& colors)
 {
 	int n = (int)rotations.size();
 
@@ -234,7 +322,7 @@ void extract_features(
 	// 读取图像，获取图像特征点并保存
 	Ptr<Feature2D> sift = cv::SIFT::create();
 	for (auto it = image_names.begin(); it != image_names.end(); ++it)
-	{	
+	{
 		// 读取图像
 		img = imread(*it);
 		if (img.empty())
@@ -261,7 +349,7 @@ void extract_features(
 		descriptor_for_all.push_back(descriptor);
 
 		// 三通道 存放该位置三通道颜色
-		vector<Vec3b> colors(key_points.size());	
+		vector<Vec3b> colors(key_points.size());
 		for (int i = 0; i < key_points.size(); ++i)
 		{
 			Point2f& kp = key_points[i].pt;
@@ -273,7 +361,7 @@ void extract_features(
 			}
 			else
 			{
-				printf("[Warning]: pt2d[%d, %d] out of image range.\n", kp.x, kp.y);
+				printf("[Warning]: pt2d[%.3f, %.3f] out of image range.\n", kp.x, kp.y);
 			}
 		}
 
@@ -340,7 +428,7 @@ void init_structure(
 	const vector<vector<KeyPoint>>& key_points_for_all,
 	const vector<vector<Vec3b>>& colors_for_all,
 	const vector<vector<DMatch>>& matches_for_all,
-	vector<Point3f>& structure,
+	vector<Point3d>& structure,
 	vector<vector<int>>& correspond_struct_idx,
 	vector<Vec3b>& colors,
 	vector<Mat>& rotations,
@@ -354,12 +442,16 @@ void init_structure(
 	Mat mask;	// mask中大于零的点代表匹配点，等于零的点代表失配点
 	get_matched_points(key_points_for_all[0], key_points_for_all[1], matches_for_all[0], p1, p2);
 	get_matched_colors(colors_for_all[0], colors_for_all[1], matches_for_all[0], colors, c2);
-	find_transform(K, p1, p2, R, T, mask);	// 三角分解得到R， T 矩阵
+
+	// 根据匹配的特征点计算本质矩阵并分解得到R,T矩阵
+	find_transform(K, p1, p2, R, T, mask);
+	//printf("mask type: %d\n", mask.type());  // mask: CV_8U(uint8)
 
 	// 对头两幅图像进行三维重建
-	maskout_points(p1, mask);
-	maskout_points(p2, mask);
-	maskout_colors(colors, mask);
+	//maskout_points(mask, p1);
+	//maskout_points(mask, p2);
+	maskout_2d_pts_pair(mask, p1, p2);
+	maskout_colors(mask, colors);
 
 	Mat R0 = Mat::eye(3, 3, CV_64FC1);
 	Mat T0 = Mat::zeros(3, 1, CV_64FC1);
@@ -464,9 +556,9 @@ bool find_transform(const Mat& K,
 	return true;
 }
 
-void maskout_points(vector<Point2f>& p1, Mat& mask)
+void maskout_points(const Mat& mask, vector<Point2f>& p1)
 {
-	vector<Point2f> p1_copy = p1;
+	vector<Point2f> p1_copy = p1;  // 此处并没有使用左值引用&, 故并不指向同一块内存地址
 	p1.clear();
 
 	for (int i = 0; i < mask.rows; ++i)
@@ -478,16 +570,37 @@ void maskout_points(vector<Point2f>& p1, Mat& mask)
 	}
 }
 
-void maskout_colors(vector<Vec3b>& p1, Mat& mask)
+void maskout_2d_pts_pair(const Mat& mask, vector<Point2f>& pts1, vector<Point2f>& pts2)
 {
-	vector<Vec3b> p1_copy = p1;
-	p1.clear();
+	vector<Point2f> pts1_copy = pts1;
+	vector<Point2f> pts2_copy = pts2;
+
+	pts1.clear();
+	pts2.clear();
+	pts1.reserve(pts1_copy.size());  // 预先分配好内存，避免push_back阶段多次分配内存
+	pts2.reserve(pts2_copy.size());
+
+	for (int i = 0; i < mask.rows; i++)
+	{
+		if (mask.at<uchar>(i) > 0)
+		{
+			pts1.push_back(pts1_copy[i]);
+			pts2.push_back(pts2_copy[i]);
+		}
+	}
+}
+
+void maskout_colors(const Mat& mask, vector<Vec3b>& pts_color)
+{
+	vector<Vec3b> p1_copy = pts_color;
+	pts_color.clear();
+	pts_color.reserve(p1_copy.size());
 
 	for (int i = 0; i < mask.rows; ++i)
 	{
 		if (mask.at<uchar>(i) > 0)
 		{
-			p1.push_back(p1_copy[i]);
+			pts_color.push_back(p1_copy[i]);
 		}
 	}
 }
@@ -495,7 +608,7 @@ void maskout_colors(vector<Vec3b>& p1, Mat& mask)
 void reconstruct(const Mat& K,
 	Mat& R1, Mat& T1, Mat& R2, Mat& T2,
 	vector<Point2f>& p1, vector<Point2f>& p2,
-	vector<Point3f>& structure)
+	vector<Point3d>& structure)
 {
 	// 两个相机的投影矩阵[R, T], triangulatePoints只支持float型
 	Mat proj1(3, 4, CV_32FC1);
@@ -528,10 +641,89 @@ void reconstruct(const Mat& K,
 	}
 }
 
+void bundle_adjustment(
+	Mat& intrinsic,
+	vector<Mat>& extrinsics,
+	vector<vector<int>>& correspond_struct_idx,
+	vector<vector<KeyPoint>>& key_points_for_all,
+	vector<Point3d>& structure
+)
+{
+	ceres::Problem problem;
+
+	// load extrinsics (rotations and motions)
+	for (size_t i = 0; i < extrinsics.size(); ++i)
+	{
+		problem.AddParameterBlock(extrinsics[i].ptr<double>(), 6);
+	}
+
+	// fix the first camera.
+	problem.SetParameterBlockConstant(extrinsics[0].ptr<double>());
+
+	// load intrinsic
+	problem.AddParameterBlock(intrinsic.ptr<double>(), 4); // fx, fy, cx, cy
+
+	// load points
+	ceres::LossFunction* loss_function = new ceres::HuberLoss(4);  // loss function make bundle adjustment robuster.
+	for (size_t img_idx = 0; img_idx < correspond_struct_idx.size(); ++img_idx)
+	{
+		vector<int>& point3d_ids = correspond_struct_idx[img_idx];
+		vector<KeyPoint>& key_points = key_points_for_all[img_idx];
+		for (size_t point_idx = 0; point_idx < point3d_ids.size(); ++point_idx)
+		{
+			int point3d_id = point3d_ids[point_idx];
+			if (point3d_id < 0)
+				continue;
+
+			Point2d observed = key_points[point_idx].pt;
+
+			// 模板参数中，第一个为代价函数的类型，第二个为代价的维度，剩下三个分别为代价函数第一第二还有第三个参数的维度
+			ceres::CostFunction* cost_function = new ceres::AutoDiffCostFunction<ReprojectCost, 2, 4, 6, 3>(new ReprojectCost(observed));
+
+			problem.AddResidualBlock(
+				cost_function,
+				loss_function,
+				intrinsic.ptr<double>(),			// Intrinsic
+				extrinsics[img_idx].ptr<double>(),	// View Rotation and Translation
+				&(structure[point3d_id].x)			// Point in 3D space
+			);
+		}
+	}
+
+	// Solve BA
+	ceres::Solver::Options ceres_config_options;
+	ceres_config_options.minimizer_progress_to_stdout = false;
+	ceres_config_options.logging_type = ceres::SILENT;
+	ceres_config_options.num_threads = 1;
+	ceres_config_options.preconditioner_type = ceres::JACOBI;
+	ceres_config_options.linear_solver_type = ceres::SPARSE_SCHUR;
+	ceres_config_options.sparse_linear_algebra_library_type = ceres::EIGEN_SPARSE;
+
+	ceres::Solver::Summary summary;
+	ceres::Solve(ceres_config_options, &problem, &summary);
+
+	if (!summary.IsSolutionUsable())
+	{
+		std::cout << "Bundle Adjustment failed." << std::endl;
+	}
+	else
+	{
+		// Display statistics about the minimization
+		std::cout << std::endl
+			<< "Bundle Adjustment statistics (approximated RMSE):\n"
+			<< " #views: " << extrinsics.size() << "\n"
+			<< " #residuals: " << summary.num_residuals << "\n"
+			<< " Initial RMSE: " << std::sqrt(summary.initial_cost / summary.num_residuals) << "\n"
+			<< " Final RMSE: " << std::sqrt(summary.final_cost / summary.num_residuals) << "\n"
+			<< " Time (s): " << summary.total_time_in_seconds << "\n"
+			<< std::endl;
+	}
+}
+
 void get_objpoints_and_imgpoints(
 	vector<DMatch>& matches,
 	vector<int>& struct_indices,
-	vector<Point3f>& structure,
+	vector<Point3d>& structure,
 	vector<KeyPoint>& key_points,
 	vector<Point3f>& object_points,
 	vector<Point2f>& image_points)
@@ -559,8 +751,8 @@ void fusion_structure(
 	vector<DMatch>& matches,
 	vector<int>& struct_indices,
 	vector<int>& next_struct_indices,
-	vector<Point3f>& structure,
-	vector<Point3f>& next_structure,
+	vector<Point3d>& structure,
+	vector<Point3d>& next_structure,
 	vector<Vec3b>& colors,
 	vector<Vec3b>& next_colors)
 {
@@ -579,7 +771,7 @@ void fusion_structure(
 		// 若该点在空间中未存在，将该点加入到结构中，且这对匹配点的空间索引都为新加入的点的索引
 		structure.push_back(next_structure[i]);
 		colors.push_back(next_colors[i]);
-		struct_indices[query_idx] = next_struct_indices[train_idx] = structure.size() - 1;
+		struct_indices[query_idx] = next_struct_indices[train_idx] = int(structure.size()) - 1;
 	}
 }
 
